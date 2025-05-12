535ed7a3ac68f4cc8f2746e6ba7d491ff30c5bad
Frank McSherry <fmcsherry@me.com>
2025-05-12 17:40:14 -0400

    Demo of bluesky stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,33 @@
 import { useQuery, useZero } from "@rocicorp/zero/react";
 import { Schema } from "../schema";
 import { useState } from "react";
+import { PostThread } from "react-bluesky-embed";
+import "./styles.css";
 
 function App() {
-  const [filter, setFilter] = useState<undefined | string>();
+
   const z = useZero<Schema, undefined>();
 
-  let messageQuery = z.query.messages;
-
-  if (filter) {
-    messageQuery = messageQuery.where("author", "=", filter);
-  }
-
-  const pastMessagesQuery = z.query.expired_message_count.one();
-  const currentMessagesQuery = z.query.current_messages;
+  let messageQuery = z.query.top_posts.orderBy("count", "desc");
 
   const [messages] = useQuery(messageQuery);
-  const [expiredCount] = useQuery(pastMessagesQuery);
-  const [currentMessages] = useQuery(currentMessagesQuery);
+  
+  console.log(messages);
 
   return (
     <>
-      <div>
-        <h1>all messages</h1>
-        <p>Filter author</p>
-        <input
-          type="text"
-          value={filter}
-          onChange={(e) => {
-            setFilter(e.target.value);
-          }}
-        />
-        <ul>
-          {messages.map((message) => (
-            <li key={message.id}>
-              <p>{message.id}</p>
-              <p>from: {message.author}</p>
-              <p>channel: {message.channel}</p>
-              <p>time: {message.time}</p>
-              <p>message: {message.message}</p>
-            </li>
-          ))}
-        </ul>
-        <h1>expired_message_count</h1>
-        <p>{expiredCount?.count}</p>
-        <h1>live messages</h1>
-        <ul>
-          {currentMessages.map((message) => (
-            <li key={message.id}>
-              <p>{message.id}</p>
-              <p>from: {message.author}</p>
-              <p>channel: {message.channel}</p>
-              <p>time: {message.time}</p>
-              <p>message: {message.message}</p>
-            </li>
-          ))}
-        </ul>
+      <div className="messagesContainer">
+        {messages.map((message) => (
+          <span className="Potato">
+            {message.count}
+            <PostThread
+              params={{
+                did: message.did,
+                rkey: message.rkey,
+              }}
+            />
+          </span>
+        ))}
       </div>
     </>
   );
